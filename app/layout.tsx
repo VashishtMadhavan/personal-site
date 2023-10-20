ac284f713ee21dd8bcc82593ed0ee29d0c215c88
Vashisht Madhavan <vashishtmadhavan@Vashs-MacBook-Pro.local>
2023-10-20 18:30:31 -0400

    adding absolute path for css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from "next/link";
 import { SidebarNav } from "@/components/sidebar-nav"
 import Header from "@/components/header"
-import './globals.css'
+import '@/app/globals.css'
 import Head from 'next/head';
 
 
